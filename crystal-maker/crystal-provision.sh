bdd5970394076cb2fe38c522bc33a868a8264922
Carl Xiong <xiongc05@gmail.com>
2016-07-22 22:50:55 +0800

    Add: install samba by default
    * Remove unnecessary sudo
    * Configure fuse to allow_others

--- crystal-maker/crystal-provision.sh
+++ crystal-maker/crystal-provision.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # NOTE by default there is no need for sudo, as vagrant runs script with sudo.
 
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm --needed --assume-installed=xclip \
-     git mosh neovim fish p7zip pass rsync sdcv the_silver_searcher tmux wget stow sshpass sshfs
-sudo ln -sv /usr/bin/nvim /usr/local/bin/vi
+pacman -Syu --noconfirm
+pacman -S --noconfirm --needed --assume-installed=xclip \
+     git mosh neovim fish p7zip pass rsync sdcv the_silver_searcher tmux wget stow sshpass sshfs samba
+ln -sv /usr/bin/nvim /usr/local/bin/vi
 
 # Set root password to 'vagrant' for convenience.
-echo 'root:vagrant' | sudo chpasswd
+echo 'root:vagrant' | chpasswd
 # Enable passwd login for root
-sudo sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo systemctl restart sshd
+sed -i -e 's/^#PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
 
-sudo chsh -s /usr/bin/fish vagrant
+chsh -s /usr/bin/fish vagrant
 
 # Default Arch keymap is not set, but the base box we use have it set to `es`
-sudo localectl set-keymap --no-convert us
+localectl set-keymap --no-convert us
 
 # the base box we used by default doesn't enable vbox service
 systemctl enable vboxservice
@@ -23,5 +23,9 @@ systemctl enable vboxservice
 # Clean up
 sudo pacman -Scc --noconfirm
 cat /dev/null > ~/.bash_history && history -c && exit
+
+# FUSE and Samba settings
+sed -i -re 's/#(user_allow_other)/\1/' /etc/fuse.conf
+# TODO samba setup
 
 # -*- buffer-file-coding-system: utf-8-unix -*-
